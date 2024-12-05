12de22d1b6f7dbaade3e42fb3b30c8cf2453e11a
nandasantos-m <nanda.machado@escola.pr.gov.br>
2024-12-05 09:52:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const avanca= document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(btton =>){
+    buttn.addEventlistener('click', function({
+        const atual = document.querySelector('.ativo');
+        const proximopasso = 'paso-'+ this.gatAttribute('data-proximo')
+
+        atual.classlist.renove('.ativo');
+        document.getElementtById(proximo).classlist.add('ativo');
+    })
+ })
+ 
+        
